e7faf62ed37a136610bd75dc45b8bb83635a0471
Jyotish Pal <paljyotish36@gmail.com>
2026-03-15 15:05:52 +0530

    delyed resolved

--- server.js
+++ server.js
@@ -9,7 +9,11 @@ connectDB();
 const app = express();
 
 app.use(
-  cors()
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
 );
 
 app.use(express.json());
