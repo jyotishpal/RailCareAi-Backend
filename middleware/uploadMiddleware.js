4c8130ce79736fa0ebaa8faa55c30fa6a75fdc78
Jyotish Pal <paljyotish36@gmail.com>
2026-03-15 19:44:32 +0530

    Cloudinary media upload integration

--- middleware/uploadMiddleware.js
+++ middleware/uploadMiddleware.js
@@ -1,12 +1,12 @@
 const multer = require("multer");
-const path = require("path");
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const cloudinary = require("../config/cloudinary");
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads/");
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname));
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: "railcareai",
+    resource_type: "auto",
   },
 });
 
